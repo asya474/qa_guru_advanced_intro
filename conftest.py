8297a71f5852fa086a8c8558b3cbca4995d52035
asya474 <asya474@gmail.com>
2024-07-22 00:39:11 +0700

    api

--- conftest.py
+++ conftest.py
@@ -1,50 +1,16 @@
-import os
 import pytest
-from dotenv import load_dotenv
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-from test_api.helper import attach_helpers
 from test_api.helper.attach_helpers import add_screenshot, add_logs, add_html, add_video, mobile_attach_video
-from test_api.helper.get_env_path import get_personal_env_path
 
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser',
-        help='Browser for test',
-        choices=['firefox', 'chrome'],
-        default='chrome'
-    )
-    parser.addoption(
-        '--browser_version',
-        help='Version of browser',
-        choices=['126.0', '125.0'],
-        default='126.0'
-    )
-
-
-def pytest_configure(config):
-    context = config.getoption("--context")
-    env_file_path = f".env.{context}"
-
-    if os.path.exists(env_file_path):
-        load_dotenv(dotenv_path=env_file_path)
-    else:
-        print(f"Warning: Configuration file '{env_file_path}' not found.")
-
-
-@pytest.fixture
-def context(request):
-    return request.config.getoption("--context")
 
 @pytest.fixture()
 def api_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "126.0",
+        "browserVersion": "122.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -64,5 +30,3 @@ def api_browser():
     add_video(browser)
 
     browser.quit()
-
-
--- microservice/microservice.py
+++ microservice/microservice.py
@@ -8,24 +8,21 @@ from models.put_update import UserUpdateRequest, UserUpdateResponse
 app = FastAPI()
 
 
-@app.delete("users/{user_id}", status_code=204)
-async def delete_user(user_id: int):
-    return {"message": "User deleted successfully"}
-
-
-@app.get("users/{user_id}}", response_model=UserResponse, status_code=200)
+@app.get("api/users/{user_id}}", response_model=UserResponse, status_code=200)
 async def get_user(user_id: int):
     return UserResponse
 
 
-@app.post("users", response_model=UserResponse, status_code=201)
-async def create_user(user: User):
-    user_id = str(random.randint(100, 999))  # Генерация случайного ID
-    created_at = datetime.utcnow().isoformat() + "Z"  # Текущая дата и время в формате ISO
-    return UserResponse(name=user.name, job=user.job, id=user_id, createdAt=created_at)
+@app.post("/api/users", response_model=UserResponse, status_code=201)
+async def create_user(user: User): # Текущая дата и время в формате ISO
+    return  UserResponse(
+        name=user.name,
+        job=user.job,
+        id=str(random.randint(100, 999)),
+        createdAt=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z",
+    )
 
-
-@app.put("users/{user_id}", response_model=UserUpdateResponse, status_code=200)
+@app.put("api/users/{user_id}", response_model=UserUpdateResponse, status_code=200)
 async def update_user(user_update: UserUpdateRequest):
     updated_at = datetime.utcnow().isoformat() + "Z"  # Текущая дата и время в формате ISO
     return UserUpdateResponse(name=user_update.name, job=user_update.job, updatedAt=updated_at)
